0e2f48a91fe772140a256d8364420fe273c143d5
Leon Kwan <kwan.leon@gmail.com>
2023-05-27 21:17:01 -0700

    nativebase package

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Box, NativeBaseProvider, Center } from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Box height={"600px"} bgColor={"blue.100"}>
+
+      <Center>Hello world hehe</Center>
+      </Box>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
